8c08df25f5e8e479a79ac9f981a94246de0e3039
AlexZ98 <57647433+AlexZ98@users.noreply.github.com>
2021-12-26 13:25:16 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import * as superheroes from 'superheroes';
+import * as supervillains from 'supervillains';
+
+var mySuperHeroName = superheroes.random();
+var myVillainName = supervillains.random();
+
+console.log(mySuperHeroName);
+console.log(myVillainName);
